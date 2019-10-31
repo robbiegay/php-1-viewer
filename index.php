e375abfbf9317d0801f41d0f818d344ebef16d0d
Robbie Gay <robbiecgay@gmail.com>
2019-10-31 09:49:51 -0400

    Trying to get curl to work

--- index.php
+++ index.php
@@ -1,77 +1,11 @@
 <?php
 
-// echo readfile("data.json");
+$ch = curl_init("http://localhost:8888/index.php?function=getAllData");
+// $fp = fopen("example_homepage.txt", "r");
 
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
 
-"http://localhost:8888/index.php?function=getAllData"
-
-// $servername = "localhost";
-// $username = "root";
-// $password = "root";
-// $dbname = "apiViewer";
-
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// $query = mysql_query("select * from foodTable", $conn);
-
-// print_r($query);
-
-// $conn->close();
-
-
-/*
-// OLD HEALTHY FOOD GENERATOR
-$file = fopen("data.json", w);
-$arr = Array();
-
- # 26 items
-$food = Array(
-    "apple", "pear", "banana", "almond", "whole wheat bread", "salmon",
-    "avocado", "broccoli", "pomegranate", "pumpkin", "ginger", "lettuce",
-    "blueberry", "raspberry", "soybean", "skim milk", "mango", "dark chocolate",
-    "lentil", "lean meat", "onion", "quinoa", "beet", "plum", "kiwi", "tuna"
-    );
-# 15 items
-$group = Array(
-    "fruit", "vegetable", "whole grain", "fish", "anti-oxidant", 
-    "micro-green", "low-fat dairy", "lean meat", "high omega-3 food", "fish oil",
-    "vitamin", "mineral", "monounsaturated fat", "low sodium food", "low sugar food"
-    );
-# 7 items
-$taste = Array(
-    "amazing", "great", "good", "okay", "bad", 
-    "so grooooooss", "like it is totally inedible"
-    );
-
-class HealthyFood {
-    function __construct($name, $group, $taste) {
-        $this->name = $name;
-        $this->group = $group;
-        $this->taste = $taste;
-    }
-
-    function message() {
-        return "This is a $this->name. A type of $this->group. It tastes $this->taste.";
-    }
-}
-
-while ($x < 100000) {
-    $food_type = rand(0, 25);
-    $food_group = rand(0, 14);
-    $food_taste = rand(0, 6);
-    $item = new HealthyFood(
-        $food[$food_type], 
-        $group[$food_group], 
-        $taste[$food_taste]
-        );
-    array_push($arr, $item);
-    $x++;
-}
-
-$json = json_encode($arr);
-
-fwrite($file, $json);
-
-echo readfile("data.json");
-*/
+curl_exec($ch);
+curl_close($ch);
+// fclose($fp);
